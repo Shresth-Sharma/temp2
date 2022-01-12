5eb0dff4523b4d87f7331864dc6bf5427ba51b7b
Shresth-Sharma <76861631+Shresth-Sharma@users.noreply.github.com>
2022-01-12 13:08:19 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,8 +1,36 @@
-// On a touchscreen device, touch
-// the canvas using one or more fingers
-// at the same time
-function draw() {
-  clear();
-  let display = touches.length + ' touches';
-  text(display, 5, 10);
+var a1;
+var t1=0;
+function setup(){
+    createCanvas(400,400);
+    a1 = createSprite(200,200,200,200);
+    a1.shapeColor=("yellow");
+}
+function draw(){
+    background("red");
+    t1 = touches.length + ' touches';
+    console.log(t1);
+    if(t1>0){
+        if(mouseX<300&&mouseX>100&&mouseY<300&&mouseY>100){
+            a1.shapeColor=("blue");
+            alert("!");
+        } 
+    }
+    
+    drawSprites();
+}
+function mouseReleased(){
+    if(mouseX<300&&mouseX>100&&mouseY<300&&mouseY>100){
+        if(mouseButton === LEFT){
+            a1.shapeColor=("orange");
+            alert("@");
+        }
+    }  
+    if(mouseX<300&&mouseX>100&&mouseY<300&&mouseY>100){
+        if(mouseButton === RIGHT){
+            a1.shapeColor=("yellow");
+            alert("@");
+            
+        }
+        
+    }  
 }
